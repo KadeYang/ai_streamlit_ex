1d92256a276c364010ed8cd1842da927f5e7b9f0
KadeYang <woong199880@gmail.com>
2025-03-12 12:03:28 +0900

    설치라이브러리 추가

---
+++ 1.st_test.py
@@ -0,0 +1,10 @@
+# streamlit 실행방법
+## streamlit 1.st_test.py
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Add title
+st.title('Uber pickups in NYC')
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ai_streamlit_ex
 ai_streamlit_ex
+
+# 설치 라이브러리
+```
+pip install openai
+pip install streamlit
+```
